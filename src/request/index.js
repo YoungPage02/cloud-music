3b03da2fd66f4614aace020199b51f7aefc929db
YoungPage02 <2193085929@qq.com>
2022-05-31 22:20:24 +0800

    更新了后端API

--- src/request/index.js
+++ src/request/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let service = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://cloud-music-api-black.vercel.app/',
   timeout: 1000
 })
 export default service
