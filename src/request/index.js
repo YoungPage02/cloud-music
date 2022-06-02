61c0470380ebda2b1a795d5f959913549744dd9d
YoungPage02 <2193085929@qq.com>
2022-06-02 12:05:11 +0800

    请求延时改为3s

--- src/request/index.js
+++ src/request/index.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 let service = axios.create({
   // baseURL: 'http://localhost:3000/',
   baseURL: 'https://cloud-music-api-liard.vercel.app/',
-  timeout: 1000
+  timeout: 3000
 })
 export default service
